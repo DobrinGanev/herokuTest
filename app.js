491de0dea9fde559b0da73d20404a094eefb0f39
Dobrin <dganev@chorstream.com>
2016-08-20 18:32:44 -0600

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var qs = require('querystring');
 var appDirect = require('./appdirect-express')
-
+var port = process.env.PORT || 8080;
 var app = express();
 app.use(logger('dev'));
 var appDirectConfig = require('./app-direct.config')
@@ -18,5 +18,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
 module.exports = app;
